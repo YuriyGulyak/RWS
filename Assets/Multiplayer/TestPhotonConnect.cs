9b6c79fe2f2f447b26f1d1b3e880cb2448fb845b
YuriyGulyak <yuriygulyak@gmail.com>
2020-07-11 00:17:36 +0300

    Tested PUN connection to master server

---
+++ Assets/Multiplayer/TestPhotonConnect.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class TestPhotonConnect : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        //PhotonNetwork.NickName = "";
+        //PhotonNetwork.GameVersion = "";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+
+    public override void OnConnectedToMaster()
+    {
+        print( "OnConnectedToMaster" );
+    }
+
+    public override void OnDisconnected( DisconnectCause cause )
+    {
+        print( "OnDisconnected: " + cause );
+    }
+}
