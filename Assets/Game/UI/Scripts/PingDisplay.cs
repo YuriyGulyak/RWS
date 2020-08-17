aa1592a7dd27afcddfd78a4b9b3b60e0d6366109
YuriyGulyak <yuriygulyak@gmail.com>
2020-08-17 16:26:35 +0300

    Added ping display

---
+++ Assets/Game/UI/Scripts/PingDisplay.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+public class PingDisplay : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI textMesh = null;
+
+    [SerializeField]
+    float updateRate = 30f;
+
+
+    float lastUpdateTime;
+    string pingFormat;
+    CultureInfo invariantCulture;
+
+
+    void Awake()
+    {
+        pingFormat = textMesh.text;
+        invariantCulture = CultureInfo.InvariantCulture;
+    }
+
+    void Update()
+    {
+        if( Time.timeScale.Equals( 0f ) )
+        {
+            return;
+        }
+
+        var time = Time.time;
+        if( time - lastUpdateTime > 1f / updateRate )
+        {
+            lastUpdateTime = time;
+
+            var ping = PhotonNetwork.GetPing();
+            textMesh.text = ping.ToString( pingFormat, invariantCulture );
+        }
+    }
+}
